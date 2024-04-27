4f47225105c9aa0ead6cc44c09a9956322918260
bobronaud <bobronaud@gmail.com>
2024-04-27 15:52:50 +0300

    add different colors on technique text

--- src/components/Techniques/index.tsx
+++ src/components/Techniques/index.tsx
@@ -9,46 +9,57 @@ const techniquesData = [
   {
     name: 'graphics-texture',
     link: 'https://drive.google.com/file/d/1X-bypmaDHLOkPrdYfjs1qQWy7eCFsnBw/preview',
+    color: 'red',
   },
   {
     name: 'painting-of-berries&leaves',
     link: 'https://drive.google.com/file/d/1JZOiauUaL2Uw7g1LhzEFDFoK9UCI1DnT/preview',
+    color: 'orange',
   },
   {
     name: 'strokes-in-a-circle',
     link: 'https://drive.google.com/file/d/1M0XB2WOnC9G2ZTQJPgVGcfqASd8uWBVF/preview',
+    color: 'yellow',
   },
   {
     name: 'composition',
     link: 'https://drive.google.com/file/d/1Eh993KYrz_cTZRjQrYeruurippWIjXNt/preview',
+    color: 'green',
   },
   {
     name: 'winter-tree',
     link: 'https://drive.google.com/file/d/1s6c_OXf1m-CkMg7ZzNiA7Z4s9cKA5qPi/preview',
+    color: 'blue',
   },
   {
     name: 'practice-drawing',
     link: 'https://drive.google.com/file/d/1KDiim4g7F19ljALiZ0GOeMXI_a_zb6hJ/preview',
+    color: 'purple',
   },
   {
     name: 'millefiori-technique',
     link: 'https://drive.google.com/file/d/18i05XUe09MsqyThxqpoNRpej_vFujP_f/preview',
+    color: 'red',
   },
   {
     name: 'space-landscape',
     link: 'https://drive.google.com/file/d/1tKfa9dA0Qjh1mw8k5g8832OhqJ0TwbQ9/preview',
+    color: 'orange',
   },
   {
     name: 'techniques-art',
     link: 'https://drive.google.com/file/d/1PXJY3LLM5YgpaA4C9YkPWzejkNPjNoGA/preview',
+    color: 'yellow',
   },
   {
     name: 'formation-of-color-perception',
     link: 'https://drive.google.com/file/d/1r1no8L4IAaPr1M_1cn9Vkjk7NMKsiHFP/preview',
+    color: 'green',
   },
   {
     name: 'right-hemisphere-drawing',
     link: 'https://drive.google.com/file/d/1ImYKuL9OTNqxWZsVMJhHbbMTOD5FDO_Y/preview',
+    color: 'blue',
   },
 ];
 
@@ -56,29 +67,32 @@ const Techniques = () => {
   const { t } = useTranslation();
 
   return (
-    <Container maxWidth={false} id='techniques-wrap'>
+    <Container maxWidth={false} id="techniques-wrap">
       <Container
-        maxWidth='md'
-        id='metodicheskiy-fond'
+        maxWidth="md"
+        id="metodicheskiy-fond"
         sx={{
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
           py: 5,
-        }}>
-        <Typography align='center' variant='h2' component='h1' sx={{ mb: 3 }}>
+        }}
+      >
+        <Typography align="center" variant="h2" component="h1" sx={{ mb: 3 }}>
           {t('techniques.header')}
         </Typography>
         <Box
-          sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start', gap: 1 }}>
+          sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start', gap: 1 }}
+        >
           {techniquesData.map((item, index) => (
             <Link
               key={index}
-              underline='hover'
-              variant='h6'
-              color='#292928'
+              underline="hover"
+              variant="h6"
+              color={item.color}
               href={item.link}
-              target='_blank'>
+              target="_blank"
+            >
               - {t(`techniques.accordion.${item.name}`)}
             </Link>
           ))}
